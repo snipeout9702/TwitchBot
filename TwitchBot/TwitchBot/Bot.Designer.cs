c92309e6bb6f5ee6ef908e26cf44740cbae236ff
Tom <littlefirestudiodevelopment@gmail.com>
2017-02-18 20:47:27 +0000

    Base bot completion
    Added all required libraries.
    Nearly finished all base code.
    Need to learn how to use SQLite to create a database for the next parts to complete.

--- TwitchBot/TwitchBot/Bot.Designer.cs
+++ TwitchBot/TwitchBot/Bot.Designer.cs
@@ -28,28 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Tabs = new System.Windows.Forms.TabControl();
+            this.ViewersTab = new System.Windows.Forms.TabControl();
             this.ConnectionTab = new System.Windows.Forms.TabPage();
+            this.OutputTextBox = new System.Windows.Forms.RichTextBox();
             this.StatusLabel = new System.Windows.Forms.Label();
             this.ConnectButton = new System.Windows.Forms.Button();
             this.ChannelTextBox = new System.Windows.Forms.TextBox();
             this.PasswordTextBox = new System.Windows.Forms.TextBox();
             this.UsernameTextBox = new System.Windows.Forms.TextBox();
             this.ChatTab = new System.Windows.Forms.TabPage();
-            this.OutputTextBox = new System.Windows.Forms.RichTextBox();
-            this.Tabs.SuspendLayout();
+            this.SendChatMessageButton = new System.Windows.Forms.Button();
+            this.SendChatMessageTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ChatTextBox = new System.Windows.Forms.RichTextBox();
+            this.ViewersTabPage = new System.Windows.Forms.TabPage();
+            this.ViewersTextBox = new System.Windows.Forms.RichTextBox();
+            this.ViewersTab.SuspendLayout();
             this.ConnectionTab.SuspendLayout();
+            this.ChatTab.SuspendLayout();
+            this.ViewersTabPage.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Tabs
+            // ViewersTab
             // 
-            this.Tabs.Controls.Add(this.ConnectionTab);
-            this.Tabs.Controls.Add(this.ChatTab);
-            this.Tabs.Location = new System.Drawing.Point(0, 0);
-            this.Tabs.Name = "Tabs";
-            this.Tabs.SelectedIndex = 0;
-            this.Tabs.Size = new System.Drawing.Size(520, 295);
-            this.Tabs.TabIndex = 0;
+            this.ViewersTab.Controls.Add(this.ConnectionTab);
+            this.ViewersTab.Controls.Add(this.ChatTab);
+            this.ViewersTab.Controls.Add(this.ViewersTabPage);
+            this.ViewersTab.Location = new System.Drawing.Point(0, 0);
+            this.ViewersTab.Name = "ViewersTab";
+            this.ViewersTab.SelectedIndex = 0;
+            this.ViewersTab.Size = new System.Drawing.Size(520, 295);
+            this.ViewersTab.TabIndex = 0;
             // 
             // ConnectionTab
             // 
@@ -66,6 +75,14 @@
             this.ConnectionTab.TabIndex = 0;
             this.ConnectionTab.Text = "Connection";
             this.ConnectionTab.UseVisualStyleBackColor = true;
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(9, 115);
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.Size = new System.Drawing.Size(497, 146);
+            this.OutputTextBox.TabIndex = 7;
+            this.OutputTextBox.Text = "";
             // 
             // StatusLabel
             // 
@@ -93,7 +110,7 @@
             this.ChannelTextBox.Name = "ChannelTextBox";
             this.ChannelTextBox.Size = new System.Drawing.Size(177, 20);
             this.ChannelTextBox.TabIndex = 4;
-            this.ChannelTextBox.Text = "Channel";
+            this.ChannelTextBox.Text = "littlefirestudio";
             // 
             // PasswordTextBox
             // 
@@ -101,7 +118,7 @@
             this.PasswordTextBox.Name = "PasswordTextBox";
             this.PasswordTextBox.Size = new System.Drawing.Size(177, 20);
             this.PasswordTextBox.TabIndex = 3;
-            this.PasswordTextBox.Text = "OAuth Code";
+            this.PasswordTextBox.Text = "oauth:no22wg0ix1e54mghtortkfqcn7ykrd";
             // 
             // UsernameTextBox
             // 
@@ -109,10 +126,14 @@
             this.UsernameTextBox.Name = "UsernameTextBox";
             this.UsernameTextBox.Size = new System.Drawing.Size(177, 20);
             this.UsernameTextBox.TabIndex = 1;
-            this.UsernameTextBox.Text = "Username";
+            this.UsernameTextBox.Text = "littlefirestudio";
             // 
             // ChatTab
             // 
+            this.ChatTab.Controls.Add(this.SendChatMessageButton);
+            this.ChatTab.Controls.Add(this.SendChatMessageTextBox);
+            this.ChatTab.Controls.Add(this.label1);
+            this.ChatTab.Controls.Add(this.ChatTextBox);
             this.ChatTab.Location = new System.Drawing.Point(4, 22);
             this.ChatTab.Name = "ChatTab";
             this.ChatTab.Padding = new System.Windows.Forms.Padding(3);
@@ -121,32 +142,81 @@
             this.ChatTab.Text = "Chat";
             this.ChatTab.UseVisualStyleBackColor = true;
             // 
-            // OutputTextBox
+            // SendChatMessageButton
             // 
-            this.OutputTextBox.Location = new System.Drawing.Point(9, 115);
-            this.OutputTextBox.Name = "OutputTextBox";
-            this.OutputTextBox.Size = new System.Drawing.Size(497, 146);
-            this.OutputTextBox.TabIndex = 7;
-            this.OutputTextBox.Text = "";
+            this.SendChatMessageButton.Location = new System.Drawing.Point(431, 243);
+            this.SendChatMessageButton.Name = "SendChatMessageButton";
+            this.SendChatMessageButton.Size = new System.Drawing.Size(75, 21);
+            this.SendChatMessageButton.TabIndex = 3;
+            this.SendChatMessageButton.Text = "Send";
+            this.SendChatMessageButton.UseVisualStyleBackColor = true;
+            this.SendChatMessageButton.Click += new System.EventHandler(this.SendChatMessageButton_Click);
+            // 
+            // SendChatMessageTextBox
+            // 
+            this.SendChatMessageTextBox.Location = new System.Drawing.Point(6, 243);
+            this.SendChatMessageTextBox.Name = "SendChatMessageTextBox";
+            this.SendChatMessageTextBox.Size = new System.Drawing.Size(419, 20);
+            this.SendChatMessageTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Chat";
+            // 
+            // ChatTextBox
+            // 
+            this.ChatTextBox.Location = new System.Drawing.Point(6, 33);
+            this.ChatTextBox.Name = "ChatTextBox";
+            this.ChatTextBox.Size = new System.Drawing.Size(500, 204);
+            this.ChatTextBox.TabIndex = 0;
+            this.ChatTextBox.Text = "";
+            // 
+            // ViewersTabPage
+            // 
+            this.ViewersTabPage.Controls.Add(this.ViewersTextBox);
+            this.ViewersTabPage.Location = new System.Drawing.Point(4, 22);
+            this.ViewersTabPage.Name = "ViewersTabPage";
+            this.ViewersTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.ViewersTabPage.Size = new System.Drawing.Size(512, 269);
+            this.ViewersTabPage.TabIndex = 2;
+            this.ViewersTabPage.Text = "Viewers";
+            this.ViewersTabPage.UseVisualStyleBackColor = true;
+            // 
+            // ViewersTextBox
+            // 
+            this.ViewersTextBox.Location = new System.Drawing.Point(8, 6);
+            this.ViewersTextBox.Name = "ViewersTextBox";
+            this.ViewersTextBox.Size = new System.Drawing.Size(498, 255);
+            this.ViewersTextBox.TabIndex = 0;
+            this.ViewersTextBox.Text = "";
             // 
             // Bot
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(523, 295);
-            this.Controls.Add(this.Tabs);
+            this.Controls.Add(this.ViewersTab);
             this.Name = "Bot";
-            this.Text = "Form1";
-            this.Tabs.ResumeLayout(false);
+            this.Text = "FireBot";
+            this.ViewersTab.ResumeLayout(false);
             this.ConnectionTab.ResumeLayout(false);
             this.ConnectionTab.PerformLayout();
+            this.ChatTab.ResumeLayout(false);
+            this.ChatTab.PerformLayout();
+            this.ViewersTabPage.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabControl Tabs;
+        private System.Windows.Forms.TabControl ViewersTab;
         private System.Windows.Forms.TabPage ConnectionTab;
         private System.Windows.Forms.Button ConnectButton;
         private System.Windows.Forms.TextBox ChannelTextBox;
@@ -155,6 +225,12 @@
         private System.Windows.Forms.TabPage ChatTab;
         private System.Windows.Forms.Label StatusLabel;
         private System.Windows.Forms.RichTextBox OutputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox ChatTextBox;
+        private System.Windows.Forms.Button SendChatMessageButton;
+        private System.Windows.Forms.TextBox SendChatMessageTextBox;
+        private System.Windows.Forms.TabPage ViewersTabPage;
+        private System.Windows.Forms.RichTextBox ViewersTextBox;
     }
 }
 
--- TwitchBot/TwitchBot/Bot.cs
+++ TwitchBot/TwitchBot/Bot.cs
@@ -5,27 +5,179 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
+using System.IO;
+using TwitchLib;
+using TwitchLib.Models.Client;
+using System.Data.SQLite;
 
 namespace TwitchBot
 {
     public partial class Bot : Form
     {
+        TwitchClient Client;
+        ConnectionCredentials Credentials;
+        string USER, PASS, CHANNEL, HOSTING, STATUS;
+        List<string> Viewers = new List<string>();
+
         public Bot()
         {
             InitializeComponent();
-            Setup();
         }
 
         void Setup()
         {
+            if (!string.IsNullOrEmpty(UsernameTextBox.Text) && !string.IsNullOrEmpty(PasswordTextBox.Text) && !string.IsNullOrEmpty(ChannelTextBox.Text))
+            {
+                USER = UsernameTextBox.Text;
+                PASS = PasswordTextBox.Text;
+                CHANNEL = ChannelTextBox.Text;
+                Credentials = new ConnectionCredentials(USER, PASS);
+                if (Client == null) { Client = new TwitchClient(Credentials, CHANNEL, '!'); }
+                if (!Client.IsConnected)
+                {
+                    // Setup all event handlers
+                    Client.OnJoinedChannel += Client_OnJoinedChannel;
+                    Client.OnMessageSent += Client_OnMessageSent;
+                    Client.OnMessageReceived += Client_OnMessageReceived;
+                    Client.OnUserBanned += Client_OnUserBanned;
+                    Client.OnUserJoined += Client_OnUserJoined;
+                    Client.OnUserLeft += Client_OnUserLeft;
+                    Client.OnConnectionError += Client_OnConnectionError;
+                    Client.OnNewSubscriber += Client_OnNewSubscriber;
+                    Client.OnChatCommandReceived += Client_OnChatCommandReceived;
+                    Client.OnHostingStarted += Client_OnHostingStarted;
+                    Client.OnHostingStopped += Client_OnHostingStopped;
+                    // Connect the client
+                    Client.Connect();
+                    // Change status identifier
+                    STATUS = "Status: Connected";
+                    StatusLabel.Text = STATUS;
+                }
+            }
+        }
+
+        private void Client_OnHostingStopped(object sender, TwitchLib.Events.Client.OnHostingStoppedArgs e)
+        {
+            Client.SendMessage($"{e.HostingChannel} has stopped hosting {HOSTING}");
+            HOSTING = string.Empty;
+        }
+
+        private void Client_OnHostingStarted(object sender, TwitchLib.Events.Client.OnHostingStartedArgs e)
+        {
+            Client.SendMessage($"{e.HostingChannel} is now hosting {e.TargetChannel} for {e.Viewers} viewers!");
+            HOSTING = e.TargetChannel;
+        }
+
+        // Place into a region after completion
+
+        private void Client_OnChatCommandReceived(object sender, TwitchLib.Events.Client.OnChatCommandReceivedArgs e)
+        {
+            switch (e.Command.Command)
+            {
+                case "test":
+                    Client.SendMessage("Test command fired!");
+                    break;
+                case "UpdateViewerList":
+                    UpdateViewersText();
+                    break;
+                default:
+                    // Action taken by all commands
+                    break;
+            }
+        }
+
+        private void Client_OnNewSubscriber(object sender, TwitchLib.Events.Client.OnNewSubscriberArgs e)
+        {
+            Client.SendMessage($"Thankyou @{e.Subscriber.Name} for subscribing!");
+        }
+
+        private void Client_OnConnectionError(object sender, TwitchLib.Events.Client.OnConnectionErrorArgs e)
+        {
+            Client.Reconnect();
+        }
+
+        private void Client_OnUserLeft(object sender, TwitchLib.Events.Client.OnUserLeftArgs e)
+        {
+            Client.SendMessage($"@{e.Username} left the stream!");
+            Viewers.Remove(e.Username);
+            UpdateViewersText();
+        }
+
+        private void Client_OnUserJoined(object sender, TwitchLib.Events.Client.OnUserJoinedArgs e)
+        {
+            Client.SendMessage($"Welcome @{e.Username} to the stream!");
+            Viewers.Add(e.Username);
+            UpdateViewersText();
+        }
+
+        void UpdateViewersText()
+        {
+            ViewersTextBox.Invoke((MethodInvoker)delegate
+            {
+                foreach (string v in Viewers)
+                {
+                    ViewersTextBox.AppendText(v);
+                }
+            });
+        }
+
+        private void Client_OnUserBanned(object sender, TwitchLib.Events.Client.OnUserBannedArgs e)
+        {
+            OutputTextBox.Invoke((MethodInvoker)delegate
+            {
+                OutputTextBox.Text = "A user was banned!\n";
+            });
+            Client.SendMessage($"@{e.Username} you have been banned from {e.Channel} for {e.BanReason}");
+        }
+
+        private void Client_OnJoinedChannel(object sender, TwitchLib.Events.Client.OnJoinedChannelArgs e)
+        {
+            OutputTextBox.Invoke((MethodInvoker)delegate
+            {
+                OutputTextBox.Text = "Join successfull!\n";
+            });
+        }
+
+        private void Client_OnMessageReceived(object sender, TwitchLib.Events.Client.OnMessageReceivedArgs e)
+        {
+            OutputTextBox.Invoke((MethodInvoker)delegate
+            {
+                OutputTextBox.AppendText($"[{e.ChatMessage.UserType}] {e.ChatMessage.DisplayName}: {e.ChatMessage.Message}\n");
+            });
+            ChatTextBox.Invoke((MethodInvoker)delegate
+            {
+                OutputTextBox.AppendText($"[{e.ChatMessage.UserType}] {e.ChatMessage.DisplayName}: {e.ChatMessage.Message}\n");
+            });
+        }
+
+        private void SendChatMessageButton_Click(object sender, EventArgs e)
+        {
+            Client.SendMessage(SendChatMessageTextBox.Text);
+            SendChatMessageTextBox.Text = string.Empty;
+        }
+
+        private void Client_OnMessageSent(object sender, TwitchLib.Events.Client.OnMessageSentArgs e)
+        {
+            OutputTextBox.Invoke((MethodInvoker)delegate
+            {
+                OutputTextBox.AppendText($"[{e.SentMessage.UserType}] {USER}: {e.SentMessage.Message}\n");
+            });
+            ChatTextBox.Invoke((MethodInvoker)delegate
+            {
+                ChatTextBox.AppendText($"[{e.SentMessage.UserType}] {USER}: {e.SentMessage.Message}\n");
+            });
         }
 
         private void ConnectButton_Click(object sender, EventArgs e)
         {
+            STATUS = "Status: Connecting";
+            StatusLabel.Text = STATUS;
+            Setup();
         }
     }
 }
