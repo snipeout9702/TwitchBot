0a5e8e4699970cef11d9d83dad03863046c42f82
Tom <littlefirestudiodevelopment@gmail.com>
2017-02-18 13:03:29 +0000

    Created base items
    Created the VS solution and began adding the connect page, elemnts added but on functional.

---
+++ TwitchBot/TwitchBot/Bot.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace TwitchBot
+{
+    partial class Bot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Tabs = new System.Windows.Forms.TabControl();
+            this.ConnectionTab = new System.Windows.Forms.TabPage();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.ChannelTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.ChatTab = new System.Windows.Forms.TabPage();
+            this.OutputTextBox = new System.Windows.Forms.RichTextBox();
+            this.Tabs.SuspendLayout();
+            this.ConnectionTab.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Tabs
+            // 
+            this.Tabs.Controls.Add(this.ConnectionTab);
+            this.Tabs.Controls.Add(this.ChatTab);
+            this.Tabs.Location = new System.Drawing.Point(0, 0);
+            this.Tabs.Name = "Tabs";
+            this.Tabs.SelectedIndex = 0;
+            this.Tabs.Size = new System.Drawing.Size(520, 295);
+            this.Tabs.TabIndex = 0;
+            // 
+            // ConnectionTab
+            // 
+            this.ConnectionTab.Controls.Add(this.OutputTextBox);
+            this.ConnectionTab.Controls.Add(this.StatusLabel);
+            this.ConnectionTab.Controls.Add(this.ConnectButton);
+            this.ConnectionTab.Controls.Add(this.ChannelTextBox);
+            this.ConnectionTab.Controls.Add(this.PasswordTextBox);
+            this.ConnectionTab.Controls.Add(this.UsernameTextBox);
+            this.ConnectionTab.Location = new System.Drawing.Point(4, 22);
+            this.ConnectionTab.Name = "ConnectionTab";
+            this.ConnectionTab.Padding = new System.Windows.Forms.Padding(3);
+            this.ConnectionTab.Size = new System.Drawing.Size(512, 269);
+            this.ConnectionTab.TabIndex = 0;
+            this.ConnectionTab.Text = "Connection";
+            this.ConnectionTab.UseVisualStyleBackColor = true;
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusLabel.Location = new System.Drawing.Point(191, 32);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(319, 37);
+            this.StatusLabel.TabIndex = 6;
+            this.StatusLabel.Text = "Status: Disconnected";
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(8, 85);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(177, 23);
+            this.ConnectButton.TabIndex = 5;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // ChannelTextBox
+            // 
+            this.ChannelTextBox.Location = new System.Drawing.Point(8, 58);
+            this.ChannelTextBox.Name = "ChannelTextBox";
+            this.ChannelTextBox.Size = new System.Drawing.Size(177, 20);
+            this.ChannelTextBox.TabIndex = 4;
+            this.ChannelTextBox.Text = "Channel";
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(8, 32);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(177, 20);
+            this.PasswordTextBox.TabIndex = 3;
+            this.PasswordTextBox.Text = "OAuth Code";
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Location = new System.Drawing.Point(8, 6);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(177, 20);
+            this.UsernameTextBox.TabIndex = 1;
+            this.UsernameTextBox.Text = "Username";
+            // 
+            // ChatTab
+            // 
+            this.ChatTab.Location = new System.Drawing.Point(4, 22);
+            this.ChatTab.Name = "ChatTab";
+            this.ChatTab.Padding = new System.Windows.Forms.Padding(3);
+            this.ChatTab.Size = new System.Drawing.Size(512, 269);
+            this.ChatTab.TabIndex = 1;
+            this.ChatTab.Text = "Chat";
+            this.ChatTab.UseVisualStyleBackColor = true;
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(9, 115);
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.Size = new System.Drawing.Size(497, 146);
+            this.OutputTextBox.TabIndex = 7;
+            this.OutputTextBox.Text = "";
+            // 
+            // Bot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 295);
+            this.Controls.Add(this.Tabs);
+            this.Name = "Bot";
+            this.Text = "Form1";
+            this.Tabs.ResumeLayout(false);
+            this.ConnectionTab.ResumeLayout(false);
+            this.ConnectionTab.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl Tabs;
+        private System.Windows.Forms.TabPage ConnectionTab;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox ChannelTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.TextBox UsernameTextBox;
+        private System.Windows.Forms.TabPage ChatTab;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.RichTextBox OutputTextBox;
+    }
+}
+
---
+++ TwitchBot/TwitchBot/Bot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TwitchBot
+{
+    public partial class Bot : Form
+    {
+        public Bot()
+        {
+            InitializeComponent();
+            Setup();
+        }
+
+        void Setup()
+        {
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
